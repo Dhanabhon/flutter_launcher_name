40ce43ad63452703939a07449ceb338f0a29a67c
Tom Dhanabhon <dhanabhon26@gmail.com>
2025-03-13 22:10:32 +0700

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 const String androidManifestFile = 'android/app/src/main/AndroidManifest.xml';
 const String iOSInfoPlistFile = 'ios/Runner/Info.plist';
-const String windowsCMakeLists = 'windows/CMakeLists.txt';
+const String windowsMainCpp = 'windows/runner/main.cpp';
 const String yamlKey = 'flutter_launcher_name';
